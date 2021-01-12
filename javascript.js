5d10550da3a3324b3039ddde6efb69aa2bd132f0
jkaho <72959522+jkaho@users.noreply.github.com>
2021-01-12 23:52:05 +1100

    incorporate covid api call into zomato api call in js

--- javascript.js
+++ javascript.js
@@ -3,19 +3,10 @@ var searchBtn = $("#search-btn");
 var searchHistoryList = $("#search-history-list");
 var resultsDiv = $("#results");
 
-var queryURL = "https://cors-anywhere.herokuapp.com/https://localcoviddata.com/covid19/v1/locations?zipCode="+searchInputEl;
-
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
-});
-
 searchBtn.on("click", function(event) {
     event.preventDefault();
+
     var searchInput = $("#zipcode").val();
-    console.log(searchInput)
 
     $.ajax({
         headers: {"user-key": "9a1b7bbdae3e31891d3b697bed7433bc"},
@@ -41,6 +32,16 @@ searchBtn.on("click", function(event) {
                 },
                 success: function(response) {
                     console.log(response);
+                    var zipcode = response.restaurants[0].restaurant.location.zipcode;
+                    var queryURL = "https://cors-anywhere.herokuapp.com/https://localcoviddata.com/covid19/v1/locations?zipCode=" + zipcode;    
+
+                    $.ajax({
+                        url: queryURL,
+                        method: "GET"
+                    }).then(function(response) {
+                        console.log(response);
+                    });
+
                     for (var i = 0; i < response.restaurants.length; i++) {
                         var resultDiv = $("<div>");
                         resultDiv.attr("id", "result-each");
