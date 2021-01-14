1625f5f18fb25d444b80a1fa2e541ec54530634b
jkaho <72959522+jkaho@users.noreply.github.com>
2021-01-14 20:54:23 +1100

    add variables for zomato city and state data, add object for state codes

--- javascript.js
+++ javascript.js
@@ -3,11 +3,80 @@ var searchBtn = $("#search-btn");
 var searchHistoryList = $("#search-history-list");
 var resultsDiv = $("#results");
 
+var states =
+  {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'American Samoa': 'AS',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'District Of Columbia': 'DC',
+    'Federated States Of Micronesia': 'FM',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Guam': 'GU',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Marshall Islands': 'MH',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Northern Mariana Islands': 'MP',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Palau': 'PW',
+    'Pennsylvania': 'PA',
+    'Puerto Rico': 'PR',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virgin Islands': 'VI',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY'
+  }
+
 searchBtn.on("click", function(event) {
     event.preventDefault();
 
     var searchInput = $("#zipcode").val();
 
+    // $.ajax({
+    //     url: "https://datausa.io/api/data?drilldowns=State&measures=Population&year=latest",
+    //     method: "GET"
+    // }).then(function(response) {
+    //     console.log(response);
+    // })
     $.ajax({
         headers: {"user-key": "9a1b7bbdae3e31891d3b697bed7433bc"},
         url: "https://developers.zomato.com/api/v2.1/locations?query=" + searchInput,
@@ -17,9 +86,12 @@ searchBtn.on("click", function(event) {
             return;
         },
         success: function(response) {
+            console.log(response)
             var entityId = response.location_suggestions[0].entity_id;
             var entityType = response.location_suggestions[0].entity_type;
-    
+            var city = response.location_suggestions[0].title.slice(0, response.location_suggestions[0].title.indexOf(","));
+            var state = response.location_suggestions[0].title.slice(response.location_suggestions[0].title.indexOf(",") + 2, response.location_suggestions[0].title.length);
+
             $.ajax({
                 headers: {
                     "Accept": "application/json",
@@ -33,8 +105,6 @@ searchBtn.on("click", function(event) {
                 success: function(response) {
                     console.log(response);
                     var zipcode = response.restaurants[0].restaurant.location.zipcode;
-                    // only the zipcode of the first result of the restaurant list is used for the covid api call
-                    // does the covid api give hotspots?
                     var queryURL = "https://cors-anywhere.herokuapp.com/https://localcoviddata.com/covid19/v1/locations?zipCode=" + zipcode;    
 
                     $.ajax({ 
@@ -65,6 +135,18 @@ searchBtn.on("click", function(event) {
                     }
                 }
             })
+
+            // $.ajax({
+            //     headers: {"user-key": "9a1b7bbdae3e31891d3b697bed7433bc"},
+            //     url: "https://developers.zomato.com/api/v2.1/geocode?lat=" + latitude + "&lon=" + longitude,
+            //     method: "GET",
+            //     error: function() {
+            //         alert("Sorry, there was an error loading the data.");
+            //         return;
+            //     },
+            //     success: function(response) {}
+            // })
+
         }
     })
 })
