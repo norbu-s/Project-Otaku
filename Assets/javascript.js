e6efbbdb2c6ef836948fc875c90896e9abffc758
jkaho <72959522+jkaho@users.noreply.github.com>
2021-01-16 23:32:36 +1100

    Add API call functionality to the search history buttons

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -159,3 +159,60 @@ searchBtn.on("click", function(event) {
 
     searchInputEl.val("");
 })
+
+// get API data on search history button click
+searchHistoryList.on("click", function(event) {
+    if (event.target.classList.contains("history-btn")) {
+        resultsDiv.empty();
+        // Zomato location API call
+        var buttonName = event.target.textContent;
+
+        $.ajax({
+            headers: {"user-key": "9a1b7bbdae3e31891d3b697bed7433bc"},
+            url: "https://developers.zomato.com/api/v2.1/locations?query=" + buttonName,
+            method: "GET",
+            error: function() {
+                alert("Sorry, there was an error loading the data.");
+                return;
+            },
+            success: function(response) {
+                var entityId = response.location_suggestions[0].entity_id;
+                var entityType = response.location_suggestions[0].entity_type;
+
+                // Zomato restaurant search API call
+                $.ajax({
+                    headers: {
+                        "Accept": "application/json",
+                        "user-key": "9a1b7bbdae3e31891d3b697bed7433bc"},
+                    url: "https://developers.zomato.com/api/v2.1/search?entity_id=" + entityId + "&entity_type=" + entityType + "&count=10",
+                    method: "GET",
+                    error: function() {
+                        alert("Sorry, there was an error loading the data.");
+                        return;
+                    },
+                    success: function(response) {
+                        for (var i = 0; i < response.restaurants.length; i++) {
+                            var resultDiv = $("<div>");
+                            resultDiv.attr("id", "result-each");
+
+                            var restaurantName = response.restaurants[i].restaurant.name;
+                            var restaurantLocation = response.restaurants[i].restaurant.location.address;
+                            var restaurantPhoneNo = response.restaurants[i].restaurant.phone_numbers;
+                            var averageCostForTwo = response.restaurants[i].restaurant.average_cost_for_two;
+                            var cuisine = response.restaurants[i].restaurant.cuisines;
+
+                            var restaurantNameDiv = $("<div>" + restaurantName + "</div>");
+                            var cuisineDiv = $("<div>" + cuisine + "Cuisine");
+                            var averageCostForTwoDiv = $("<div>" + "Average Cost For Two: $" + averageCostForTwo + "</div>");
+                            var restaurantLocationDiv = $("<div>" + restaurantLocation + "</div>");
+                            var restaurantPhoneNoDiv = $("<div>" + restaurantPhoneNo + "</div>");
+
+                            resultDiv.append(restaurantNameDiv, cuisineDiv, averageCostForTwoDiv, restaurantLocationDiv, restaurantPhoneNoDiv);
+                            resultsDiv.append(resultDiv);
+                        }
+                    }
+                })
+            }
+        })
+    }
+})
