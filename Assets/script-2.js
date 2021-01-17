71d43ff1abca66d03b7a236f878639397cf1a02e
jkaho <72959522+jkaho@users.noreply.github.com>
2021-01-18 01:46:55 +1100

    Add renderImages() function when fave card divs are rendered

--- Assets/script-2.js
+++ Assets/script-2.js
@@ -1,6 +1,53 @@
 var storedImages = [];
 initialise();
 
+// Creating card from local storage information 
+var cardCounter = 1;
+for (var i = storedFaves.length - 1; i >= 0; i--) {
+    // Individual card div 
+    var faveCard = $("<div class='fave card'></div>");
+
+    // Card heading 
+    var cardHeadingDiv = $("<div id='divider" + cardCounter + "' class='fave-name card-divider'>" + storedFaves[i].name + "</div>");
+
+    // Map section
+    var viewMapBtn = $("<button id='map-btn" + cardCounter + "' class='map-btn hide'>View on Map</button>");
+    var mapDiv = $("<div class='card-section map hide'></div>");
+    var mapFrame = $("<iframe id='map" + cardCounter + "' width='100%' height='100%' frameborder='0' style='border:0' src=''></iframe>")
+    mapDiv.append(mapFrame);
+    
+    // Image section
+    var cardImgDiv = $("<div id='img-div" + cardCounter + "' class='card-section img hide'></div>");
+    var uploadImgForm = $("<form action='/action_page.php'></form>");
+    var uploadImgBtn = $("<input type='file' id='imgInput" + cardCounter + "' accept='image/*'>");
+    uploadImgForm.append(uploadImgBtn);
+    var img = $("<img id='img" + cardCounter + "' src='#'>");
+    cardImgDiv.append(uploadImgForm, img);
+    
+    // Info section
+    var cardInfo = $("<div id='info" + cardCounter + "' class='card-section info hide'></div>");
+    var cuisine = $("<div class='cuisine'><strong>Cuisine: </strong>" + storedFaves[i].cuisine + "</div>");
+    var cost = $("<div class='cost'><strong>Average Cost For Two: </strong>" + storedFaves[i].cost + "</div>");
+    var address = $("<div class='location'><strong>Address: </strong>" + storedFaves[i].location + "</div>");
+    var phone = $("<div class='phone'><strong>Phone: </strong>" + storedFaves[i].phone + "</div>");
+    cardInfo.append(cuisine, cost, address, phone);
+    
+    // Notes section
+    var cardNotes = $("<div id='notes" + cardCounter + "' class='card-section notes hide'></div>");
+    var noteLabel = $("<label for='note-input" + cardCounter + "'>Notes:</label>");
+    var noteTextArea = $("<textarea type='text' id='note-input" + cardCounter + "' placeholder='Personal notes'></textarea>");
+    cardNotes.append(noteLabel, noteTextArea);
+
+    // Append heading and sections to individual card
+    faveCard.append(cardHeadingDiv, viewMapBtn, mapDiv, cardImgDiv, cardInfo, cardNotes);
+    
+    // Append individual card to <div class="cell">
+    $(".cell").append(faveCard);
+    cardCounter++;
+
+    renderImages();
+}
+
 // functionality for 'upload image' button
 var cardNumber = 0;
 
@@ -22,8 +69,8 @@ $(function() {
 });
 
 function imageIsLoaded(e) {
-    $("#uploadImg" + cardNumber).removeAttr("class");
-    $("#uploadImg" + cardNumber).attr("src", e.target.result);
+    $("#img" + cardNumber).removeAttr("class");
+    $("#img" + cardNumber).attr("src", e.target.result);
     for (var i = 0; i < storedImages.length; i++) {
         if (storedImages[i][0] === cardNumber) {
             storedImages.splice(i, 1);
@@ -37,7 +84,7 @@ function imageIsLoaded(e) {
 // local storage for saved imgs
 function renderImages() {
     for (var i = 0; i < storedImages.length; i++) {
-        $("#uploadImg" + storedImages[i][0]).attr("src", storedImages[i][1]);
+        $("#img" + storedImages[i][0]).attr("src", storedImages[i][1]);
     }
 }
 
@@ -74,51 +121,6 @@ document.body.addEventListener("click", function(event) {
     }
 })
 
-// Creating card from local storage information 
-var cardCounter = 1;
-for (var i = storedFaves.length - 1; i >= 0; i--) {
-    // Individual card div 
-    var faveCard = $("<div class='fave card'></div>");
-
-    // Card heading 
-    var cardHeadingDiv = $("<div class='fave-name card-divider'>" + storedFaves[i].name + "</div>");
-
-    // Map section
-    var viewMapBtn = $("<button id='map-btn" + cardCounter + "' class='map-btn hide'>View on Map</button>");
-    var mapDiv = $("<div class='card-section map hide'></div>");
-    var mapFrame = $("<iframe id='map" + cardCounter + "' width='100%' height='100%' frameborder='0' style='border:0' src=''></iframe>")
-    mapDiv.append(mapFrame);
-    
-    // Image section
-    var cardImgDiv = $("<div id='img-div" + cardCounter + "' class='card-section img hide'></div>");
-    var uploadImgForm = $("<form action='/action_page.php'></form>");
-    var uploadImgBtn = $("<input type='file' id='imgInput" + cardCounter + "' accept='image/*'>");
-    uploadImgForm.append(uploadImgBtn);
-    var img = $("<img id='img" + cardCounter + "' src='#'>");
-    cardImgDiv.append(uploadImgForm, img);
-    
-    // Info section
-    var cardInfo = $("<div id='info" + cardCounter + "' class='card-section info hide'></div>");
-    var cuisine = $("<div class='cuisine'><strong>Cuisine: </strong>" + storedFaves[i].cuisine + "</div>");
-    var cost = $("<div class='cost'><strong>Average Cost For Two: </strong>" + storedFaves[i].cost + "</div>");
-    var address = $("<div class='location'><strong>Address: </strong>" + storedFaves[i].location + "</div>");
-    var phone = $("<div class='phone'><strong>Phone: </strong>" + storedFaves[i].phone + "</div>");
-    cardInfo.append(cuisine, cost, address, phone);
-    
-    // Notes section
-    var cardNotes = $("<div id='notes" + cardCounter + "' class='card-section notes hide'></div>");
-    var noteLabel = $("<label for='note-input" + cardNumber + "'>Notes:</label>");
-    var noteTextArea = $("<textarea type='text' id='note-input" + cardNumber + "' placeholder='Personal notes'></textarea>");
-    cardNotes.append(noteLabel, noteTextArea);
-
-    // Append heading and sections to individual card
-    faveCard.append(cardHeadingDiv, viewMapBtn, mapDiv, cardImgDiv, cardInfo, cardNotes);
-    
-    // Append individual card to <div class="cell">
-    $(".cell").append(faveCard);
-    cardCounter++;
-}
-
 // Expanding and minimising card
 var cardStatus = "closed";
 $(".card-divider").on("click", function(event) {
@@ -127,14 +129,14 @@ $(".card-divider").on("click", function(event) {
         cardStatus = "opened";
         $("#map-btn" + cardNumber).removeClass("hide");
         $("#map-btn" + cardNumber).text("View on Map");
-        $("#img" + cardNumber).removeClass("hide");
+        $("#img-div" + cardNumber).removeClass("hide");
         $("#info" + cardNumber).removeClass("hide");
         $("#notes" + cardNumber).removeClass("hide");
     } else {
         cardStatus = "closed";
         $("#map-btn" + cardNumber).addClass("hide");
         $("#map" + cardNumber).parent().addClass("hide");
-        $("#img" + cardNumber).addClass("hide");
+        $("#img-div" + cardNumber).addClass("hide");
         $("#info" + cardNumber).addClass("hide");
         $("#notes" + cardNumber).addClass("hide");
     }
